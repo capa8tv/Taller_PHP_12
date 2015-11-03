81bd8746e8450e7c23f4b1f7e6bff85a4bd3b1db
Capa8.tv <canalcapa8@gmail.com>
2015-11-02 22:51:57 -0400

    jquery.php

---
+++ jquery.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" /> 
+        <title>Ejemplo de Ajax con Capa8.tv #manosenelcódigo</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script type="text/javascript" src="js/funciones.js"></script>
+    </head>
+    <body>
+      <h1>Ejemplo de Ajax con Capa8.tv #manosenelcódigo</h1>
+      <a href="javascript:void(0);" onclick="carga_ajax('12','contenedor','respuesta_ajax_2.php');">Haz Clic</a>
+      <hr />
+      <div id="contenedor">
+        hola valor inicial con ñandú
+      </div>
+    </body>
+</html>
