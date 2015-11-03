f6bf965e4e6ee82a6014d97fa981db3e156ee105
Capa8.tv <canalcapa8@gmail.com>
2015-11-02 22:51:25 -0400

    listado.php

---
+++ listado.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" /> 
+        <title>Ejemplo de Ajax con Capa8.tv #manosenelcódigo</title>
+        <script type="text/javascript" src="js/funciones.js"></script>
+    </head>
+    <body>
+      <h1>Ejemplo de Ajax con Capa8.tv #manosenelcódigo</h1>
+      <a href="javascript:void(0);" onclick="ajax('12','contenedor','respuesta_ajax.php');">Haz Clic</a>
+      <hr />
+      <div id="contenedor">
+        hola valor inicial con ñandú
+      </div>
+    </body>
+</html>
+
+me gustó la clase de PHP y Ajax desde @capa8tv #manosenelcódigo
